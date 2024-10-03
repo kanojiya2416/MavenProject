dd342db3597768121dbbb1d89004d699f8a8c641
nites <nites@DESKTOP-25E90KT>
2024-10-04 01:34:58 +0530

    webdriver

---
+++ src/test/java/seleniumwebdriver/MavenDemo.java
@@ -0,0 +1,28 @@
+package seleniumwebdriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class MavenDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Login");
+//			ChromeDriver driver=new ChromeDriver();
+			WebDriver driver=new ChromeDriver();
+			
+			driver.get("https://www.google.com");
+			
+			String title = driver.getTitle();
+			if(title.equals("Google")) {
+				System.out.println("Test Passed");
+			}else {
+				System.out.println("Test Failed");
+			}
+			
+			driver.close();
+			
+	}
+
+}
+	
